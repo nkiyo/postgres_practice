b73290f7c8e1bd63e43fe563fd621290fd350262
root <root@rocky8.localdomain>
2023-10-26 00:18:59 +0000

    up

--- setup_cms.sql
+++ setup_cms.sql
@@ -1,16 +1,3 @@
----- User
---create user "cms";
---alter role cms password 'password';
---
----- Database
---create database "cmsdb" encoding 'UTF8';
---
----- Schema
----- => \dn で確認可能
---\c cmsdb
---create schema cms authorization cms;
---GRANT ALL ON SCHEMA cms TO cms;
-
 -- Table
 create table cms.users (
   NAME CHAR(64),
